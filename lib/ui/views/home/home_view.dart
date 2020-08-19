27d2b868a62f264b5f4a64e062b6616ee1430ec7
jamescardona11 <jjcardonao@gmail.com>
2020-08-19 18:15:55 -0500

    Init app to learn about stacked architecture

---
+++ lib/ui/views/home/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_example_app/ui/views/home/home_viewmodel.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<HomeViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text(model.title),
+        ),
+      ),
+      viewModelBuilder: () => HomeViewModel(),
+    );
+  }
+}
